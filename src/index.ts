e44ce3e0740cb4ebf1db55fe84aede35aef128e2
ekhaled <me.khaled@gmail.com>
2022-09-09 14:14:31 +0100

    more exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { jsColorTools } from './bulma-color-tools'
+import { bulmaColorTools } from './bulma-color-tools'
 
 export { ColorUpdater } from './color-updater'
 export { newRgb as rgb, newHsl as hsl } from './bulma-color-tools'
 
-const { darken, lighten } = jsColorTools
-export { darken, lighten }
+const { darken, lighten, rgba, transparentize } = bulmaColorTools
+export { darken, lighten, rgba, transparentize, bulmaColorTools }
